26ddb0ac38a560990a8103f7d14152cb51c8a34c
Isaac Moreno <isaace.mp99@gmail.com>
2020-10-12 21:51:51 -0500

    Avance1
    Diseño y funcionalidad de las ventanas

---
+++ Avance1/JS/noticiajs.js
@@ -0,0 +1,11 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.ctn-date', { delay: 500});
+ScrollReveal().reveal('.ctn-up', { delay: 500});
+ScrollReveal().reveal('.image-one', { delay: 500});
+ScrollReveal().reveal('.ctn-down', { delay: 500});
+ScrollReveal().reveal('.image-two', { delay: 500});
+ScrollReveal().reveal('.ctn-footer', { delay: 500});
